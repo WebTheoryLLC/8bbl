6ee7143cca1793d9721163b866abd799ecf3904c
William Holt <william@toastedlabs.com>
2014-04-23 00:51:35 -0500

    Removed email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:facebook, :twitter],
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook, :twitter],
 				 :authentication_keys => [:login]
   after_save :create_game_list
 

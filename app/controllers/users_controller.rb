575c8b2fe6ba90c8b8806e05e9e4c0776fd334f8
William Holt <william@toastedlabs.com>
2014-04-23 00:05:36 -0500

    added mandrill, user updating and login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,25 @@ class UsersController < ApplicationController
     else
       @games = current_user.gamelist.gamelistgames
     end
+    if user_signed_in?
+      @giantbomb_suggestions = current_user.gamelist.suggested_games
+    end
   end
+  
+  def edit
+    @user = current_user  
+  end
+  
+  def update
+    @user = current_user
+    @user.update_attributes(user_params)
+    redirect_to settings_path, notice: "Updated Settings!"
+  end
+  
+  private
+  
+  def user_params 
+    params.require(:user).permit(:username, :first_name, :last_name, :bio, :twitter_handle, :giant_bomb_handle, :twitch_handle, :email)
+  end
+  
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Eightbitbacklog::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :port =>          '587',
+  :address =>        'http://smtp.mandrillapp.com',
+  :user_name =>      ENV['MANDRILL_USERNAME'],
+  :password =>      ENV['MANDRILL_APIKEY'],
+  :domain =>        'http://heroku.com',
+  :authentication => :plain
+}

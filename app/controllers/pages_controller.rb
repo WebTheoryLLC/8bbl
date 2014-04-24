ee1cf6523ca54a81b7366837f5be78eccde092d0
Jonathan Gertig <jcgertig@gmail.com>
2014-04-24 15:38:30 -0500

    add stats

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -67,7 +67,18 @@ class PagesController < ApplicationController
   end
 
   def stats
+    @user_count = User.all.count
+    @games_count = Game.all.count
+    @genre_count = Genre.all.count
+    @concept_count = Concept.all.count
+    @platform_count = Platform.all.count
+    @developer_count = Developer.all.count
+
+    @games_beaten_count = 0
+    @games_in_progress_count = 0
+
     @gamelistgames = Gamelistgame.all
+    @gamelistgames_count = @gamelistgames.count
 
     @top_games = {}
     @top_game_genres = {}
@@ -94,25 +105,27 @@ class PagesController < ApplicationController
       end
 
       if gamelistgame.status == "Beaten"
+        @games_beaten_count += 1
         @top_games_beaten[@game.name] = @top_games_beaten[@game.name] ? @top_games_beaten[@game.name] + 1 : 1
       elsif gamelistgame.status == "In Progress"
+        @games_in_progress_count += 1
         @top_games_playing_now[@game.name] = @top_games_playing_now[@game.name] ? @top_games_playing_now[@game.name] + 1 : 1
       end
     end
 
-    @top_games["No Game Stats"] = 1 if @top_games.count = 0
-    @top_game_genres["No Genre Stats"] = 1 if @top_game_genres.count = 0
+    @top_games["No Game Stats"] = 1 if @top_games.count == 0
+    @top_game_genres["No Genre Stats"] = 1 if @top_game_genres.count == 0
     @top_games_beaten["No Games Beaten"] = 1 if @top_games_beaten.count == 0
-    @top_game_platforms["No Platform Stats"] = 1 if @top_game_platforms.count = 0
-    @top_game_developers["No Developer Stats"] = 1 if @top_game_developers.count = 0
+    @top_game_platforms["No Platform Stats"] = 1 if @top_game_platforms.count == 0
+    @top_game_developers["No Developer Stats"] = 1 if @top_game_developers.count == 0
     @top_games_playing_now["No Games In Progress Now"] = 1 if @top_games_playing_now.count == 0
 
-    @top_games             = [["Game", "Quantity of Games"]]      + group_for_graph(@top_games, 10, false)
-    @top_game_genres       = [["Genre", "Quantity of Games"]]     + group_for_graph(@top_game_genres, 10, false)
-    @top_games_beaten      = [["Game", "Quantity of Games"]]      + group_for_graph(@top_games_beaten, 10, false)
-    @top_game_platforms    = [["Platform", "Quantity of Games"]]  + group_for_graph(@top_game_platforms, 10, false)
+    @top_games             = [["Game",      "Quantity of Games"]] + group_for_graph(@top_games, 10, false)
+    @top_game_genres       = [["Genre",     "Quantity of Games"]] + group_for_graph(@top_game_genres, 10, false)
+    @top_games_beaten      = [["Game",      "Quantity of Games"]] + group_for_graph(@top_games_beaten, 10, false)
+    @top_game_platforms    = [["Platform",  "Quantity of Games"]] + group_for_graph(@top_game_platforms, 10, false)
     @top_game_developers   = [["Developer", "Quantity of Games"]] + group_for_graph(@top_game_developers, 10, false)
-    @top_games_playing_now = [["Game", "Quantity of Games"]]      + group_for_graph(@top_games_playing_now, 10, false)
+    @top_games_playing_now = [["Game",      "Quantity of Games"]] + group_for_graph(@top_games_playing_now, 10, false)
   end
 
   def user_signin_up

45b3632a92fd76eb5ebff77ff5094224f1524e63
Jonathan Gertig <jcgertig@gmail.com>
2014-04-24 22:11:26 -0500

    fix for page refresh everytime

--- app/controllers/gamelistgames_controller.rb
+++ app/controllers/gamelistgames_controller.rb
@@ -1,14 +1,14 @@
 class GamelistgamesController < ApplicationController
 	before_action :authenticate_user!
-  
+
   def update
     @game = Gamelistgame.find(params[:gamelistgame][:id])
     @game.update_attributes(gamelistgame_params)
-    redirect_to gamelist_path  
+    # redirect_to gamelist_path  
   end
-  
+
   private
-  
+
   def gamelistgame_params
     params.require(:gamelistgame).permit(:status)
   end
